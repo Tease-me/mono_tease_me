5ca4662f60ca54c0a0ec3d9e7e7e5df07161b972
Shirish Koirala <shirish.mjpro@gmail.com>
2025-07-23 10:04:47 +1000

    HTTPS setup

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,22 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import fs from 'fs';
 
 export default defineConfig({
-  plugins: [tailwindcss(), react(), svgr(),],
+  plugins: [tailwindcss(), react(), svgr() ],
   server: {
     port: 3000,
+    host: true,
+    open: true,
+    https: {
+      key: fs.readFileSync('./.cert/key.pem'),
+      cert: fs.readFileSync('./.cert/cert.pem'),
+    },
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
-  },
+  }
 });
