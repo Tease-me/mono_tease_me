f60ea5e00f8431a073603074ada450047978e142
Shirish Koirala <shirish.mjpro@gmail.com>
2025-08-07 10:23:01 +1000

    Call Hook Added to VoiceCall

--- src/ui/screens/messaging/VoiceCallEleven.tsx
+++ src/ui/screens/messaging/VoiceCallEleven.tsx
@@ -1,48 +1,39 @@
+import React, { useEffect, useState } from "react";
 import BackgroundGradient from "@/ui/templates/BackgroundGradient";
 import CenteredLayout from "@/ui/templates/CenteredLayout";
-import { useConversation } from "@11labs/react";
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import styles from "./VoiceCallEleven.module.css";
 import CallIcon from "@/assets/Call.svg?react";
 import CloseSquareIcon from "@/assets/CloseSquare.svg?react";
 import WifiIcon from "@/assets/Wifi.svg?react";
-import NoSignalIcon from "@/assets/svg/NoSignal.svg";
+import NoSignalIcon from "@/assets/svg/NoSignal.svg?react";
 import { InfluencerDataModel } from "@/data/models/InfluencerDataModel";
 import { InfluencerRepo } from "@/data/repositories/InfluencerRepo";
-import { useMicrophonePermission } from "@/hooks/useMicrophonePermission";
 import ProfileMedia from "@/ui/components/ProfileMedia";
 import CircularIconButton from "@/ui/components/inputs/buttons/CircularIconButton";
 import LoadingSpinner from "@/ui/components/loading/LoadingSpinner";
 import OnBoardingTopNav from "@/ui/components/nav/OnBoardingTopNav";
 import { truncateLastName } from "@/utils/StringUtils";
 import { useLocation, useNavigate } from "react-router-dom";
-import { apiClient } from "@/api/apis";
-import { ChatServices } from "@/api/services/ChatServices";
-import { ChatRepository } from "@/data/repositories/ChatRepo";
+import useCall from "@/hooks/useCall";
+
+import styles from "./VoiceCallEleven.module.css";
 
 interface VoiceCallElevenProps { }
 
 const VoiceCallEleven: React.FC<VoiceCallElevenProps> = ({ }) => {
-  const [isRecording, setIsRecording] = useState(false);
-  const [status, setStatus] = useState<string>("Online");
-  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
-  const [isConnected, setIsConnected] = useState(false);
   const [isOnline, setIsOnline] = useState<boolean>(navigator.onLine);
   const [influencerId, setInfluencerId] = useState<string | undefined>();
-  const {
-    permissionState,
-    requestMicrophonePermission,
-    releaseMicrophonePermission,
-  } = useMicrophonePermission();
-
-  const navigate = useNavigate();
   const [influencer, setInfluencer] = useState<InfluencerDataModel>();
-  const ringtoneRef = useRef(new Audio("/audio/ringtone.wav"));
+
+  const { status, startConversation, stopConversation } = useCall(influencer!);
   const { state } = useLocation();
 
   const influencerRepo = InfluencerRepo();
-  const chatRepo = ChatRepository();
+
+  useEffect(() => {
+    setIsOnline(navigator.onLine)
+  }, [navigator.onLine])
 
   useEffect(() => {
     const { influencer_id } = state as { influencer_id: string };
@@ -60,75 +51,8 @@ const VoiceCallEleven: React.FC<VoiceCallElevenProps> = ({ }) => {
     })();
   }, [influencerId]);
 
-  const ring = () => {
-    const ringtone = ringtoneRef.current;
-    ringtone.loop = true;
-
-    ringtone.play().catch((err) => {
-      console.error("Ringtone playback failed:", err);
-    });
-  };
-
-  const stopRing = () => {
-    ringtoneRef.current.pause();
-    ringtoneRef.current.currentTime = 0;
-  };
-
-  const conversation = useConversation({
-    onConnect: () => {
-      console.log("connected");
-      stopRing();
-      setIsConnected(true);
-      setStatus("Connected");
-      setIsRecording(true);
-    },
-    onDisconnect: () => {
-      console.log("disconnected");
-      setIsConnected(false);
-      setIsRecording(false);
-      setStatus("Disconnected");
-    },
-    onError: (error) => {
-      console.log(error);
-      setError("An error occurred during the conversation");
-      setIsConnected(false);
-      setIsRecording(false);
-      setStatus("Error");
-    },
-    onMessage: (message) => {
-      console.log(message);
-    },
-  });
-
-  async function startConversation() {
-    ring();
-    const hasPermission = await requestMicrophonePermission();
-    if (!hasPermission) {
-      alert("No permission");
-      return;
-    }
-    if (!influencerId) {
-      alert("Influencer ID is missing");
-      stopRing();
-      return;
-    }
-
-    const signedUrl = await chatRepo.getSignedUrl(influencerId);
-    if (!signedUrl) {
-      stopRing();
-      return;
-    }
-    const conversationId = await conversation.startSession({ signedUrl });
-    console.log(conversationId);
-  }
-  const stopConversation = useCallback(async () => {
-    await conversation.endSession();
-    releaseMicrophonePermission();
-  }, [conversation]);
-
   const handleVoiceToggle = async () => {
-    if (isRecording) {
-      setStatus("Disconnecting...");
+    if (status === "connected") {
       stopConversation();
     } else {
       startConversation();
@@ -159,28 +83,17 @@ const VoiceCallEleven: React.FC<VoiceCallElevenProps> = ({ }) => {
               <CircularIconButton
                 onClick={handleVoiceToggle}
                 disabled={isLoading}
-                icon={isConnected ? <CloseSquareIcon /> : <CallIcon />}
+                icon={status === "connected" ? <CloseSquareIcon /> : <CallIcon />}
               />
               <div className="h-12 flex items-center justify-center">
-                {isRecording ? (
+                {status === "connected" ? (
                   <div className="flex justify-center items-center space-x-1">
-                    {[...Array(12)].map((_, i) => (
-                      <div
-                        key={i}
-                        className="w-1 bg-white/30 rounded-full animate-pulse"
-                        style={{
-                          height: `${Math.max(12, Math.random() * 48)}px`,
-                          animationDelay: `${i * 0.1}s`,
-                          animationDuration: "0.5s",
-                        }}
-                      />
-                    ))}
                   </div>
                 ) : (
                   !isLoading && (
                     <div className={styles["network-status-container"]}>
                       {isOnline ? (
-                        <WifiIcon className="h-4 w-4" />
+                        <WifiIcon />
                       ) : (
                         <NoSignalIcon />
                       )}
@@ -192,9 +105,9 @@ const VoiceCallEleven: React.FC<VoiceCallElevenProps> = ({ }) => {
                 )}
               </div>
 
-              {error && (
-                <div className="text-red-300/90 text-sm bg-red-500/10 p-4 rounded-2xl backdrop-blur-sm">
-                  {error}
+              {status === "error" && (
+                <div>
+                  "Something went wrong!"
                 </div>
               )}
             </div>
