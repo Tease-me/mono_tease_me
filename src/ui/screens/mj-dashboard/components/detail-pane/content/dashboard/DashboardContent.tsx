3e0679434fc844ccab777342f92aa2a99de6fc51
Shirish Koirala <shirish.mjpro@gmail.com>
2025-08-13 13:13:18 +1000

    Stats card Completed

--- src/ui/screens/mj-dashboard/components/detail-pane/content/dashboard/DashboardContent.tsx
+++ src/ui/screens/mj-dashboard/components/detail-pane/content/dashboard/DashboardContent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from "./DashboardContent.module.css"
 import DashboardCard from './components/DashboardCard';
+import DashboardStatsCard from './components/stats-card/DashboardStatsCard';
 
 interface DashboardContentProps {
 }
@@ -10,17 +11,17 @@ const DashboardContent: React.FC<DashboardContentProps> = ({ }) => {
         <>
             <div className={styles["dashboard-content"]}>
                 <div className={styles["stat-section"]}>
-                    <DashboardCard className={styles.chart}>Chart Section</DashboardCard>
-                    <DashboardCard>Stats Section</DashboardCard>
-                    <DashboardCard>Stats Section</DashboardCard>
-                    <DashboardCard>Stats Section</DashboardCard>
-                    <DashboardCard>Stats Section</DashboardCard>
-                    <DashboardCard>Stats Section</DashboardCard>
+                    <DashboardStatsCard title='Earning Data' className={styles.chart}>Chart Section</DashboardStatsCard>
+                    <DashboardStatsCard title='Total Users'>123</DashboardStatsCard>
+                    <DashboardStatsCard title='Total New Users'>3</DashboardStatsCard>
+                    <DashboardStatsCard title='Total Influencers'>5</DashboardStatsCard>
+                    <DashboardStatsCard title='Total Issues Reported'>3</DashboardStatsCard>
+                    <DashboardStatsCard title='Total Chats'>150</DashboardStatsCard>
                 </div>
 
                 <div className={styles["list-section"]}>
-                    <DashboardCard >Chart Section</DashboardCard>
-                    <DashboardCard >Stats Section</DashboardCard>
+                    <DashboardCard>Top Influencer Earners</DashboardCard>
+                    <DashboardCard>Top Users Spend</DashboardCard>
                 </div>
             </div>
         </>
--- src/ui/screens/mj-dashboard/components/detail-pane/content/dashboard/components/stats-card/DashboardStatsCard.tsx
+++ src/ui/screens/mj-dashboard/components/detail-pane/content/dashboard/components/stats-card/DashboardStatsCard.tsx
@@ -1,16 +1,20 @@
-import React, { ReactNode } from 'react';
+import React, { HTMLAttributes } from 'react';
 import styles from "./DashboardStatsCard.module.css"
+import DashboardCard from '../DashboardCard';
+import clsx from 'clsx';
 
-interface DashboardStatsCardProps {
+interface DashboardStatsCardProps extends HTMLAttributes<HTMLDivElement> {
     title: string;
-    children: ReactNode;
 }
 
-const DashboardStatsCard: React.FC<DashboardStatsCardProps> = ({ }) => {
+const DashboardStatsCard: React.FC<DashboardStatsCardProps> = ({ title, ...props }) => {
     return (
-        <div className={styles["dashboard-stats-card"]}>
-
-        </div>
+        <DashboardCard className={clsx(props.className, styles["dashboard-stats-card"])} >
+            <div className={styles["title"]}>{title}</div>
+            <div className={styles["content"]}>
+                {props.children}
+            </div>
+        </DashboardCard>
     );
 };
 
