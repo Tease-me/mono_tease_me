3df7ba64611cc89538349ce548d848b89f9dbf6c
Shirish Koirala <shirish.mjpro@gmail.com>
2025-08-14 10:50:59 +1000

    Bar Chart Added.

--- src/mj-dashboard/ui/components/detail-pane/content/dashboard/DashboardContent.tsx
+++ src/mj-dashboard/ui/components/detail-pane/content/dashboard/DashboardContent.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import styles from "./DashboardContent.module.css"
 import DashboardStatsCard from './components/cards/stats-card/DashboardStatsCard';
 import DashboardListCard from './components/cards/list-card/DashboardListCard';
 import { UserRepo } from '@/data/repositories/UserRepo';
 import { UserDataModel } from '@/data/models/UserDataModel';
 import DashboardListCardItem from './components/cards/list-card/DashboardListCardItem';
 
+import styles from "./DashboardContent.module.css"
+import DashboardBarChartCard from './components/cards/stats-card/DashboardBarChartCard';
 interface DashboardContentProps {
 }
 
@@ -13,6 +14,20 @@ const DashboardContent: React.FC<DashboardContentProps> = ({ }) => {
     const [users, setUsers] = useState<UserDataModel[]>();
 
     const userRepo = UserRepo();
+    const data = [
+        { name: 'Jan', uv: 235 },
+        { name: 'Feb', uv: 500 },
+        { name: 'Mar', uv: 250 },
+        { name: 'Apr', uv: 200 },
+        { name: 'May', uv: 300 },
+        { name: 'Jun', uv: 400 },
+        { name: 'July', uv: 350 },
+        { name: 'Aug', uv: 450 },
+        { name: 'Sept', uv: 330 },
+        { name: 'Oct', uv: 200 },
+        { name: 'Nov', uv: 500 },
+        { name: 'Dec', uv: 450 },
+    ];
 
     useEffect(() => {
         (async () => {
@@ -20,10 +35,11 @@ const DashboardContent: React.FC<DashboardContentProps> = ({ }) => {
             setUsers(response);
         })()
     }, [])
+
     return (
         <div className={styles["dashboard-content"]}>
             <div className={styles["stat-section"]}>
-                <DashboardStatsCard title='Earning Data' className={styles.chart}>Chart Section</DashboardStatsCard>
+                <DashboardBarChartCard title='Earning Data' className={styles.chart} data={data} />
                 <DashboardStatsCard title='Total Users'>123</DashboardStatsCard>
                 <DashboardStatsCard title='Total New Users'>3</DashboardStatsCard>
                 <DashboardStatsCard title='Total Influencers'>5</DashboardStatsCard>
@@ -33,10 +49,10 @@ const DashboardContent: React.FC<DashboardContentProps> = ({ }) => {
 
             <div className={styles["list-section"]}>
                 <DashboardListCard title='Top Influencer Earners'>
-                    {users?.map((user) => <DashboardListCardItem title={user.name ?? ""} cost='$10' imgUrl={user.imgUrl} />)}
+                    {users?.map((user) => <DashboardListCardItem key={user.id} title={user.name ?? ""} cost='$10' imgUrl={user.imgUrl} />)}
                 </DashboardListCard>
                 <DashboardListCard title='Top Users Spend'>
-                    {users?.map((user) => <DashboardListCardItem title={user.name ?? ""} cost='$10' imgUrl={user.imgUrl} />)}
+                    {users?.map((user) => <DashboardListCardItem key={user.id} title={user.name ?? ""} cost='$10' imgUrl={user.imgUrl} />)}
                 </DashboardListCard>
             </div>
         </div>
---
+++ src/mj-dashboard/ui/components/detail-pane/content/dashboard/components/cards/stats-card/DashboardBarChartCard.tsx
@@ -0,0 +1,40 @@
+import React, { HTMLAttributes, useEffect, useRef, useState } from 'react';
+import styles from "./DashboardBarChartCard.module.css"
+import clsx from 'clsx';
+import DashboardCard from '../DashboardCard';
+import { Bar, BarChart, XAxis } from 'recharts';
+
+interface DashboardBarChartCardProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
+    title: string;
+    data: any[];
+}
+
+const DashboardBarChartCard: React.FC<DashboardBarChartCardProps> = (({ title, data, ...props }) => {
+    const [dimensions, setDimensions] = useState({ width: 100, height: 300 });
+    const barchartCardRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        function updateSize() {
+            if (!barchartCardRef.current) return;
+            const { width, height } = barchartCardRef.current.getBoundingClientRect();
+            setDimensions({ width: width, height: height });
+        }
+        updateSize();
+        window.addEventListener('resize', updateSize);
+        return () => window.removeEventListener('resize', updateSize);
+    }, [barchartCardRef])
+
+    return (
+        <DashboardCard {...props} className={clsx(styles["dashboard-stats-card"], props.className)} >
+            <div className={styles["title"]}>{title}</div>
+            <div ref={barchartCardRef} className={styles["content"]}>
+                <BarChart width={dimensions.width} height={dimensions.height} data={data}>
+                    <XAxis dataKey="name" />
+                    <Bar dataKey="uv" barSize={dimensions.width * 0.05} fill="var(--color-primary)" />
+                </BarChart>
+            </div>
+        </DashboardCard>
+    );
+});
+
+export default DashboardBarChartCard;
