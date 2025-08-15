8b4cd3ae76e155bb4cb0ac3d1f54396c8695b2b9
Shirish Koirala <shirish.mjpro@gmail.com>
2025-08-15 13:24:41 +1000

    Sorting and Small Fixes

--- src/dummy/influencers.ts
+++ src/dummy/influencers.ts
@@ -1,6 +1,7 @@
 import { DashboardInfluencerModel } from "@/mj-dashboard/data/models/DashboardInfluencerModel";
 import dummy from "./dummy";
 import { AccountStatus, SubscriptionLevel } from "@/mj-dashboard/data/models/enums";
+import { getRandomEnumValue } from "@/utils/enum_utils";
 
 export async function makeDashboardInfluencer(gender: "female" | "male" = "female"): Promise<DashboardInfluencerModel> {
     const fullName = gender === "female"
@@ -10,10 +11,10 @@ export async function makeDashboardInfluencer(gender: "female" | "male" = "femal
         imgUrl: await dummy.image.getRandomFemaleProfilePictures(),
         fullName: fullName,
         earnings: Math.floor(20000 + Math.random() * 40000),
-        accountStatus: AccountStatus.active,
+        accountStatus: getRandomEnumValue(AccountStatus),
         id: dummy.generateRandomId(),
-        joinedDate: dummy.formatDateDDMMYYYY(),
-        subscriptionLevel: SubscriptionLevel.basic,
+        joinedDate: dummy.formatDateDDMMYYYY(dummy.getRandomDate()),
+        subscriptionLevel: getRandomEnumValue(SubscriptionLevel),
         username: dummy.makeUsername(fullName),
         isSelected: false
     })
