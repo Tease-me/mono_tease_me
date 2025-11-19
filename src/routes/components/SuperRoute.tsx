b81ff3bf4e5c25c7c9ca1075e90417ae98bda165
Shirish Koirala <shirish.mjpro@gmail.com>
2025-11-20 08:43:30 +1000

    Super Route created

---
+++ src/routes/components/SuperRoute.tsx
@@ -0,0 +1,26 @@
+import { AuthContext } from "@/context/AuthContext";
+import React, { ReactNode, useContext } from "react";
+import { Navigate } from "react-router-dom";
+import BlockingLoader from "@/ui/components/loading/BlockingLoader";
+
+interface SuperRouteProps {
+    children: ReactNode;
+}
+
+const SuperRoute: React.FC<SuperRouteProps> = ({ children }) => {
+    const { isSignedIn, loadingAuth, user } = useContext(AuthContext);
+
+    if (loadingAuth) {
+        return (
+            <BlockingLoader />
+        );
+    }
+
+    if (!isSignedIn && user?.id !== 1) {
+        return <Navigate to="/home" />;
+    }
+
+    return <>{children}</>;
+};
+
+export default SuperRoute;
