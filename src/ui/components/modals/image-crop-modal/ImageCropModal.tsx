5d9e562b4c2d869abc50ab5b2c442fd34bb4bc79
manishmjpro <manish.mjpro@gmail.com>
2026-01-06 10:42:02 +1000

    Image Crop Modal created

---
+++ src/ui/components/modals/image-crop-modal/ImageCropModal.tsx
@@ -0,0 +1,33 @@
+import { Modal } from "@/ui/components/modals/Modal";
+import React, { useEffect, useRef, useState } from "react";
+
+import styles from './ImageCrop.module.css';
+
+
+export interface ImageCropProps {
+    isOpen: boolean;
+    imageSrc: string;
+    onClose: () => void;
+}
+
+const ImageCrop: React.FC<ImageCropProps> = ({
+    isOpen,
+    imageSrc,
+    onClose
+}) => {
+    return (
+        <Modal isOpen={isOpen} onClose={onClose} className={styles.modal} >
+            <div className={styles.container} >
+                <div className={styles.header}>
+                    <h3>Crop Image</h3>
+                </div>
+                <div className={styles.imageArea}>
+                </div>
+
+            </div>
+        </Modal>
+    )
+}
+
+export default ImageCrop;
+
