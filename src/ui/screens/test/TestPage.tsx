9289dab2b5f72f5989224ef6ecf6bd5df2a00bf0
Shirish Koirala <shirish.mjpro@gmail.com>
2026-01-12 11:24:57 +1000

    Small fixes

--- src/ui/screens/test/TestPage.tsx
+++ src/ui/screens/test/TestPage.tsx
@@ -1,16 +1,12 @@
 import FullWidthLayout from '@/ui/templates/FullWidthLayout';
-import { useState } from 'react';
-import AdultModeToggle from '@/ui/components/adult-mode-toggle/AdultModeToggle';
+import ButtonsTestPage from './ButtonsTestPage';
 
 
 const TestPage = ({ }) => {
-    const [toggleState, setToggleState] = useState(false);
-
     return (
         <FullWidthLayout>
-            <div style={{ padding: 10, width: 200 }}>
-                <AdultModeToggle checked={toggleState} onChange={() => setToggleState(!toggleState)} />
-            </div>
+
+            <ButtonsTestPage />
         </FullWidthLayout>
     );
 };
