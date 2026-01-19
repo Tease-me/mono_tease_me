2970c53d6a9860fd7c4097f12e056d95569ce6b6
manishmjpro <manish.mjpro@gmail.com>
2026-01-19 16:23:28 +1000

    Payment Result component foundation

---
+++ src/ui/components/payment/PaymentResult.tsx
@@ -0,0 +1,20 @@
+
+type PaymentResultProps = {
+  success: boolean,
+  onBack : () => void;
+  onContactSupport: () => void;
+  amount: number;
+  influencerName: string;
+}
+
+
+export default function PaymentResult({success, onBack, onContactSupport, amount, influencerName}: PaymentResultProps) {
+
+return(
+  <>
+  
+  </>
+
+)
+
+}
