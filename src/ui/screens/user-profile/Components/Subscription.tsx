004088c915318ee5caeefe7d346a1691bb931474
manishmjpro <manish.mjpro@gmail.com>
2026-01-28 15:34:48 +1000

    added subsription plan data model

--- src/ui/screens/user-profile/Components/Subscription.tsx
+++ src/ui/screens/user-profile/Components/Subscription.tsx
@@ -11,9 +11,15 @@ const Subscription = ({ goTo, navPayload, goBack }: SubscriptionProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.cards}>
-        <PricingPlanCard title="Basic" price="200.00" callTime="100 min" />
-        <PricingPlanCard title="Plus" price="149.00" callTime="200 min" active />
-        <PricingPlanCard title="Premium" price="500.00" callTime="600 min" />
+        <div className={styles.card}>
+          <PricingPlanCard title="Basic" price="200.00" callTime="100 min" />
+        </div>
+        <div className={styles.card}>
+          <PricingPlanCard title="Plus" price="149.00" callTime="200 min" active />
+        </div>
+        <div className={styles.card}>
+          <PricingPlanCard title="Premium" price="500.00" callTime="600 min" />
+        </div>
       </div>
     </div>
   );
