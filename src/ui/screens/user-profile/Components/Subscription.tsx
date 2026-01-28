dcd54fc2c86580449ed58c8b6f2a880e5663cabb
manishmjpro <manish.mjpro@gmail.com>
2026-01-28 16:36:49 +1000

    added useQuery to manage sbuscription plan fetch onceo nly

--- src/ui/screens/user-profile/Components/Subscription.tsx
+++ src/ui/screens/user-profile/Components/Subscription.tsx
@@ -1,5 +1,12 @@
+import { useQuery } from "@tanstack/react-query"
+import { SubscriptionsServices } from "@/api/services/SubscriptionsServices";
+import { apiClient } from "@/api/apis";
+import clsx from "clsx"
+
 import PricingPlanCard from "@/ui/components/cards/PricingPlanCard";
 import styles from "./Subscription.module.css";
+import LoadingSpinner from "@/ui/components/loading/LoadingSpinner";
+
 
 type SubscriptionProps = {
   goTo: (id: string, payload?: Record<string, any>) => void;
@@ -7,22 +14,44 @@ type SubscriptionProps = {
   goBack?: () => void;
 };
 
-const Subscription = ({ goTo, navPayload, goBack }: SubscriptionProps) => {
+
+
+
+const Subscription = ({ }: SubscriptionProps) => {
+
+  const subscriptionPlanSvc = SubscriptionsServices(apiClient)
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["subscriptionPlans"],
+    queryFn: () => subscriptionPlanSvc.getPlans(),
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    refetchOnMount: false,
+  }
+  );
+
+  const recurringPlans = data?.recurring ?? [];
+
   return (
-    <div className={styles.container}>
+    <div className={clsx(styles.container, "u-sidebar-page")} >
+      {isLoading && <div className={styles.loading}><LoadingSpinner /></div>}
+      {error && <div className={styles.error}>Couldn’t load plans</div>}
       <div className={styles.cards}>
-        <div className={styles.card}>
-          <PricingPlanCard title="Basic" price="200.00" callTime="100 min" />
-        </div>
-        <div className={styles.card}>
-          <PricingPlanCard title="Plus" price="149.00" callTime="200 min" active />
-        </div>
-        <div className={styles.card}>
-          <PricingPlanCard title="Premium" price="500.00" callTime="600 min" />
-        </div>
+        {recurringPlans.map((plan) => (
+          <div key={plan.id} className={styles.card}>
+            <PricingPlanCard
+              title={plan.name}
+              price={(plan.price_cents / 100).toFixed(2)}
+              callTime={`${plan.features.minutes_equivalent ?? 0} min`}
+              active={plan.is_featured}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
+
 };
 
 export default Subscription;
