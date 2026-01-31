494814e60879c841304ef2d4ef63ec2c95c3d2ba
manishmjpro <manish.mjpro@gmail.com>
2026-01-31 12:56:55 +1000

    url redirect

--- src/api/services/AdultVerificationServices.ts
+++ src/api/services/AdultVerificationServices.ts
@@ -1,38 +1,31 @@
 import { Endpoints } from "../urls";
-import { apiClient } from "../apis";
+import { AxiosInstance } from "axios";
 
-
-export const startVerificationSession = async () => {
-
-  const response = await apiClient.post(Endpoints.verification.session, "kyc");
-  return response.data;
-}
-
-export const getVerificationSessionStatus = async (sessionId: string) => {
-  const response = await apiClient.get(Endpoints.verification.sessionStatus(sessionId));
-  return response.data;
-};
-
-export const completeVerificationSession = async (sessionId: string) => {
-  const response = await apiClient.post(Endpoints.verification.sessionComplete(sessionId));
-  return response.data;
-};
-
-
-export const getVerificationStatus = async () => {
-  const response = await apiClient.get(Endpoints.verification.status)
-  return response.data;
-}
-
-
-export const getVerificationHistory = async () => {
-  const response = await apiClient.get(Endpoints.verification.history)
-  return response.data;
-}
-
-
-export const verificationWebhook = async () => {
-  const response = await apiClient.get(Endpoints.verification.webhook)
-  return response.data;
-}
-
+export const AdultVerificationSerivces = (apiClient: AxiosInstance) => ({
+  startVerificationSession: async () => {
+    const response = await apiClient.post(Endpoints.verification.session, {
+      workflow_type: "kyc",
+    });
+    return response.data;
+  },
+  getVerificationSessionStatus: async (sessionId: string) => {
+    const response = await apiClient.get(Endpoints.verification.sessionStatus(sessionId));
+    return response.data;
+  },
+  completeVerificationSession: async (sessionId: string) => {
+    const response = await apiClient.post(Endpoints.verification.sessionComplete(sessionId));
+    return response.data;
+  },
+  getVerificationStatus: async () => {
+    const response = await apiClient.get(Endpoints.verification.status)
+    return response.data;
+  },
+  getVerificationHistory: async () => {
+    const response = await apiClient.get(Endpoints.verification.history)
+    return response.data;
+  },
+  verificationWebhook: async () => {
+    const response = await apiClient.get(Endpoints.verification.webhook)
+    return response.data;
+  }
+});
