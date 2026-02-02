358205fede50521429a30cca312e8af12a931727
Shirish Koirala <shirish.mjpro@gmail.com>
2026-02-02 11:46:35 +1000

    Create Account Updated

--- src/ui/screens/register/RegisterScreen.tsx
+++ src/ui/screens/register/RegisterScreen.tsx
@@ -122,7 +122,7 @@ export default function RegisterScreen() {
   };
 
   const handleBackClick = () => {
-    navigate("/");
+    navigate(Paths.influencerProfile(username || ""));
   };
   const handleContinueClicked = () => {
     handleSubmit();
