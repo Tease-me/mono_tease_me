761781fa3f20a2a05d965e125ba6c8504f8e9562
GlaucoInfinities <glaucomp@hotmail.com>
2026-05-14 11:00:48 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,10 +11,12 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import posthog from "posthog-js";
 import { PostHogErrorBoundary, PostHogProvider } from "@posthog/react";
+import { IS_PRODUCTION } from "./env";
 
 posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN, {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: "2026-01-30",
+  opt_out_capturing_by_default: !IS_PRODUCTION,
 });
 
 function cleanupPwaArtifacts() {
